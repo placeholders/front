2e7a2f0831ee50f40e66925b13084a7c44b87bad
daillen <andre.v712@gmail.com>
2018-05-05 18:30:01 -0300

    database access done

--- backend/main.py
+++ backend/main.py
@@ -3,10 +3,27 @@ import json
 import http.client
 
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
+
+class TestTable(db.Model):
+    __tablename__ = 'test'
+
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    name = db.Column(db.String())
+
+    def __init__(self, name):
+        self.name = name
 
 @app.route('/')
 def main():
     return 'test'
+
+@app.route('/name/<name>')
+def test(name):
+    db.session.add(TestTable(name))
+    db.session.commit()
+    return name
---
+++ backend/set_env.sh
@@ -0,0 +1,2 @@
+export FLASK_APP=main.py
+export DATABASE_URL=mysql://root:root@localhost:3306/test
