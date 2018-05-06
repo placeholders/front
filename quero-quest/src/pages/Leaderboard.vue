6ac658ee1b9f2e70f512d4b739b621f00feded12
Vinícius Hoyer <vhoyer@live.com>
2018-05-06 12:35:21 +0000

    forgot the file

---
+++ quero-quest/src/pages/Leaderboard.vue
@@ -0,0 +1,35 @@
+
+<template>
+<div id="main">
+    <header>
+        <NavApp />
+    </header>
+    <main>
+        <LeaderBoard />
+    </main>
+</div>
+
+</template>
+
+<style scoped>
+#quests {
+    display: flex;
+    align-items: stretch;
+    flex-flow: column nowrap;
+
+    margin: 0 auto;
+}
+#quests > * {
+    margin-bottom: 2.5rem;
+}
+</style>
+
+<script>
+import LeaderBoard from '@/components/app-leaderboard.vue'
+
+export default {
+    components:{
+        LeaderBoard,
+    }
+}
+</script>
