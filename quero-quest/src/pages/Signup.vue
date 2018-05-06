da7094079fdadbbb0118055a48647bafa9658f86
Vinícius Hoyer <vhoyer@live.com>
2018-05-06 06:53:51 +0000

    basic signup wiredup

--- quero-quest/src/pages/Signup.vue
+++ quero-quest/src/pages/Signup.vue
@@ -1,5 +1,9 @@
 <template>
   <div id="login">
+      <vk-notification
+        status="danger"
+        :messages.sync="messages"
+        />
       <vk-card class="uk-width-1-5@m uk-position-absolute uk-transform-center" style="left:50%;top:50%">
           <vk-card-title slot="header">
                 Sign up
@@ -9,6 +13,14 @@
                     input-id="txtLogin"
                     input-placeholder="my awesome nickname"
                     label-text="Username"
+                    v-model="username"
+                    />
+
+                <FormInput
+                    input-id="txtLogin"
+                    input-placeholder="my awesome fullname"
+                    label-text="Your name:"
+                    v-model="fullname"
                     />
 
                 <FormInput
@@ -16,6 +28,7 @@
                     input-type="password"
                     input-placeholder="*******"
                     label-text="Password"
+                    v-model="password"
                     />
 
                 <FormInput
@@ -23,10 +36,12 @@
                     input-type="password"
                     input-placeholder="*******"
                     label-text="Confirm your password"
+                    v-model="confpass"
                     />
 
                 <div class="uk-margin" uk-margin>
                     <button
+                        @click="signup()"
                         class="uk-button uk-button-primary"
                         type="button">
                         sign up
@@ -38,9 +53,46 @@
 </template>
 
 <script>
+import axios from 'axios'
 import FormInput from '@/components/form-input.vue'
 
 export default {
+    data() {return{
+        fullname: "",
+        username: "",
+        password: "",
+        confpass: "",
+
+        messages: [],
+    }},
+    methods: {
+        validate: function(){
+            if ([this.fullname,this.username,this.password,this.confpass].indexOf("") != -1){
+                this.messages.push("Fill all fields")
+                return false
+            }
+            return true
+        },
+        signup: function(){
+            if (!this.validate()){
+                return;
+            }
+
+            axios({
+                method: 'post',
+                url: '127.0.0.1:5000/user/register',
+                data: {
+                    name: this.fullname,
+                    login: this.username,
+                    password: this.password,
+                    confirmed_password: this.confpass,
+                },
+            })
+            .then(response => {
+                this.messages.push(response.data)
+            })
+        }
+    },
     components: {
         FormInput
     }
