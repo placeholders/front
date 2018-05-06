f37fcb23bbe4ab1377ddc8b88d933df1e6ca11c8
daillen <andre.v712@gmail.com>
2018-05-05 21:45:45 -0300

    Database models done?

--- backend/main.py
+++ backend/main.py
@@ -5,25 +5,19 @@ import http.client
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+from models import *
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-db = SQLAlchemy(app)
 
-class TestTable(db.Model):
-    __tablename__ = 'test'
-
-    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-    name = db.Column(db.String())
-
-    def __init__(self, name):
-        self.name = name
+db.init_app(app)
 
 @app.route('/')
 def main():
-    return 'test'
+    return 'running'
 
-@app.route('/name/<name>')
-def test(name):
-    db.session.add(TestTable(name))
+@app.route('/test/<name>/<passwd>')
+def test(name, passwd):
+    db.session.add(UserTable(name, passwd))
     db.session.commit()
-    return name
+    return name, passwd
