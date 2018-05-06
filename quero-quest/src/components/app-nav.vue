1e79e414e1300b5cafd6bcc23fecc6c84c923493
Vinícius Hoyer <vhoyer@live.com>
2018-05-06 03:55:46 +0000

    Prepared navbar to navigate

--- quero-quest/src/components/app-nav.vue
+++ quero-quest/src/components/app-nav.vue
@@ -5,13 +5,31 @@
         <img src="https://placehold.it/60" />
       </vk-navbar-logo>
 
-      <vk-navbar-nav-item title="Profile" ></vk-navbar-nav-item>
-      <vk-navbar-nav-item title="Problem" ></vk-navbar-nav-item>
-      <vk-navbar-nav-item title="Problem report" ></vk-navbar-nav-item>
-      <vk-navbar-nav-item title="Leader Board" ></vk-navbar-nav-item>
+      <vk-navbar-nav-item
+        title="Profile"
+        />
+      <vk-navbar-nav-item
+        title="Quests"
+        />
+      <vk-navbar-nav-item
+        title="Post a Quest"
+        />
+      <vk-navbar-nav-item
+        title="Leaderboard"
+        />
     </vk-navbar-nav>
     <vk-navbar-nav slot="right" class="uk-padding-small">
       <vk-button type="primary">Login</vk-button>
     </vk-navbar-nav>
   </vk-navbar>
 </template>
+
+<script>
+export default {
+  methods:{
+    navigateTo: function(to){
+      window.router.push(to)
+    }
+  }
+}
+</script>
