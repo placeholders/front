b79853d521940fa7529b322dd70e52a87d5b8244
Vinícius Hoyer <vhoyer@live.com>
2018-05-06 08:07:07 +0000

    Login wired up

--- quero-quest/src/pages/Login.vue
+++ quero-quest/src/pages/Login.vue
@@ -1,5 +1,9 @@
 <template>
   <div id="login">
+      <vk-notification
+        status="danger"
+        :messages.sync="messages"
+        />
       <vk-card class="uk-width-1-5@m uk-position-absolute uk-transform-center" style="left:50%;top:50%">
           <vk-card-title slot="header">
                 Login
@@ -9,6 +13,7 @@
                     input-id="txtLogin"
                     input-placeholder="my awesome nickname"
                     label-text="Username"
+                    v-model="user"
                     />
 
                 <FormInput
@@ -16,11 +21,13 @@
                     input-type="password"
                     input-placeholder="*******"
                     label-text="Password"
+                    v-model="pass"
                     />
 
                 <div class="uk-margin" uk-margin>
                     <button
                         class="uk-button uk-button-primary"
+                        @click="login()"
                         type="button">
                         Login
                     </button>
@@ -39,13 +46,47 @@
 </template>
 
 <script>
+import axios from 'axios'
 import FormInput from '@/components/form-input.vue'
 
 export default {
+    data(){return{
+        messages: [],
+        user: "",
+        pass: "",
+    }},
     methods: {
         navigateTo: function(to){
             window.router.push(to)
         },
+        validade: function(){
+            if ([this.user,this.pass].indexOf("") !== -1){
+                this.messages.push("Fill all the fields")
+                return false
+            }
+            return true
+        },
+        login: function(){
+            if (!this.validate()){
+                return
+            }
+
+            axios({
+                method: 'post',
+                url: 'https://127.0.0.1/login',
+                data: {
+                    login: this.user,
+                    password: this.pass,
+                }
+            }).then(response => {
+                if(response.data !== "success"){ 
+                    this.messages.push( response.data )
+                    return
+                }
+
+                this.navigateTo("/")
+            })
+        }
     },
     components: {
         FormInput
