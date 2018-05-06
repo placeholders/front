bec7dbb9a8bfb9e03becdb9fe48ee705b4e7f6e1
Vinícius Hoyer <vhoyer@live.com>
2018-05-06 13:27:35 +0000

    Centralized

--- quero-quest/src/pages/Quest.vue
+++ quero-quest/src/pages/Quest.vue
@@ -2,7 +2,7 @@
 <div>
     <AppNav />
 
-    <div class="uk-width-2-3@m">
+    <div id="container" class="uk-width-2-3@m">
         <QuestCard
             :key="$route.params.id"
             :user="quest.creator"
@@ -53,42 +53,11 @@ export default {
 
 
 <style scoped>
-.body {
-  display: grid;
-  grid-template:
-    "title desc" 1fr
-    "votes desc" 1fr
-    / 1fr 2fr
-    
+#container {
+    display: flex;
+    align-items: stretch;
+    flex-flow: column nowrap;
 
-}
-.body .title {
-  grid-area: title;
-
-}
-.body .title > * { 
-	font-size: 1.3rem;
-  font-weight: bold;
-}
-.small-text {
-  font-size: 0.95rem;
-}
-.body .votes {
-  grid-area: votes;
-}
-.body .votes2 {
-  grid-area: votes2;
-}
-.body .votes3 {
-  grid-area: votes3;
-}
-.body .desc {
-  grid-area: desc;
-}
-#voted{
-  padding-left: 32.5%;
-}
-#votet{
-  padding-left: 65%;
+    margin: 0 auto;
 }
 </style>
