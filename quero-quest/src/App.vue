6cf26d0eab1b6921e3327f9b4443054567c2f9ce
Vinícius Hoyer <vhoyer@live.com>
2018-05-06 09:44:24 +0000

    save and load from storage

--- quero-quest/src/App.vue
+++ quero-quest/src/App.vue
@@ -9,7 +9,17 @@ export default {
       login: "",
     }
   }},
+  watch:{
+    user: function(){
+      window.sessionStorage.setItem("user", this.user)
+    }
+  },
   mounted: function(){
+    this.user = window.sessionStorage.getItem("user")
+
+    if (this.user == null){
+      window.router.push("/login")
+    }
   }
 }
 </script>
