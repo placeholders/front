07dc2a11fd2f91cd5227f9d898e6fa241b6cb099
Vinícius Hoyer <vhoyer@live.com>
2018-05-06 02:12:04 +0000

    Separated the container to quests in its own components

---
+++ quero-quest/src/components/quest-container.vue
@@ -0,0 +1,12 @@
+<template>
+<div id="quests">
+    <vk-card>
+        <vk-card-title>wololo</vk-card-title>
+    </vk-card>
+
+    <vk-card>
+        <vk-card-title>wololo</vk-card-title>
+        <vk-label slot="badge" type="success">new</vk-label>
+    </vk-card>
+</div>
+</template>
--- quero-quest/src/pages/Home.vue
+++ quero-quest/src/pages/Home.vue
@@ -1,17 +1,15 @@
 <template>
 <div id="main">
-    <vk-card>
-        <vk-card-title>wololo</vk-card-title>
-    </vk-card>
-
-    <vk-card>
-        <vk-card-title>wololo</vk-card-title>
-        <vk-label slot="badge" type="success">new</vk-label>
-    </vk-card>
+    <QuestContainer />
 </div>
 </template>
 
 <script>
+import QuestContainer from '@/components/quest-container.vue'
+
 export default {
+    components:{
+        QuestContainer,
+    }
 }
 </script>
